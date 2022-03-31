c0afba93f05e790d46d6793b4a24b779d8482dde
mvarrone <mativarrone2@gmail.com>
2022-03-30 21:25:17 -0300

    Changes on README.md

--- backend/deploy_to_nginx.py
+++ backend/deploy_to_nginx.py
@@ -52,22 +52,23 @@ def get_api_json():
 
         write_info('postman/openapi.json', response.text)
 
-        postman_text = """How to import API documentation into Postman
+        postman_text = """# How to import API documentation into Postman
 
-        1) Open Postman
-        2) Go to "APIs", select "Create an API", select "Import" tab
-        3) Under "Import local schema files", hit "Select files" Button
-        4) Select openapi.json located inside postman directory
-        5) On next page, click on Import button and then on Close button
-        6) API docs is imported on Collections and APIs Tabs. Use what you like
-        7) Go to FastAPIDocumentation > draft > FastAPIDocumentation > Variables
-        8) Set baseUrl:
-            VARIABLE: baseUrl
-            INITIAL VALUE: /
-            CURRENT VALUE: """ + base_url + """
-            Be sure this field is checked
-        9) Click on Save Button (upper right located)
-        10) Now, you can test every endpoint
+        1. Open Postman
+        2. Go to "APIs", select "New", inside "Advanced" section select "API" and then "Import" tab
+        3. Under "Import local schema files", hit "Select files" Button
+        4. Select openapi.json located inside postman directory
+        5. On next page, click on Import button and then on Close button
+        6. API docs is imported on Collections and APIs Tabs. Use what you like
+        7. Go to FastAPIDocumentation > draft > FastAPIDocumentation > Variables
+        8. Set baseUrl:
+        - VARIABLE: baseUrl
+        - INITIAL VALUE: /
+        - CURRENT VALUE: """ + base_url + """
+        - Be sure this field is checked
+        9. Click on Save Button (upper right located)
+        10. Now, you can test every endpoint
+
             Example: {{baseUrl}}/logs
         
         """
