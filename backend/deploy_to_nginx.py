f918d35c28c96cec0f16e91a64791a2d2554a641
Matias Varrone <mativarrone2@gmail.com>
2022-04-05 22:36:49 -0300

    Changes to postman/REAMDE.md 5

--- backend/deploy_to_nginx.py
+++ backend/deploy_to_nginx.py
@@ -105,12 +105,12 @@ def get_api_json():
     - On "tfc" collection, go to "Authorization" tab and select:
 
         Type: Bearer Token
-
         Token: {{token_tfc}}
 
 3. Configure endpoints to inherit auth from parent collection
 
     - For every endpoint:
+
         Go to "Authorization" tab and select "Inherit auth from parent" (in Type section)
     
     Under this, you will see "This request is using Bearer Token from collection tfc."
